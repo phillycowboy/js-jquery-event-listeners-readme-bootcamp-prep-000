f162f9d1e61d8d892c56499a21d7e79c26a1d449
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-26 19:27:42 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 
 
 function pressIt(){
-  $('input').on('keydown',function(){
+  $('input').on('keydown',function(key){
     if(key.which===71){
       window.alert("G was pressed");
     }
